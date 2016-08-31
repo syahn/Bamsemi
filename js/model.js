5481e549262cb4a505793434593c4788c3f17306
SY <sy.ahn25@gmail.com>
2016-08-31 12:18:55 +0900

    feat: Add cutomized marker icon and geolocation icon

---
+++ js/model.js
@@ -0,0 +1,34 @@
+var locations = [
+    {
+        class: 'hollys',
+        title: '연신내점',
+        location: {lat: 37.617136, lng: 126.919693},
+        contact: "02-999-0000",
+        img: "http://admin.hollys.co.kr/upload/branch/store_201606140435158310.jpg"
+    },
+    {
+        class: 'hollys',
+        title: '건대일감호수점',
+        location: {lat: 37.539161, lng: 127.075555}
+
+    },
+    {
+        class: 'hollys',
+        title: '안암오거리점',
+        location: {lat: 37.583626, lng: 127.0293}
+    },
+    {
+        class: 'hollys',
+        title: '노원문화의거리점',
+        location: {lat: 37.655852, lng: 127.063961}
+    },
+    {
+        class: 'hollys',
+        title: '도곡점',
+        location: {lat: 37.484489, lng: 127.046183}
+    }
+];
+
+var logos = {
+    "hollys" : "img/logo_hollys.png"
+};
